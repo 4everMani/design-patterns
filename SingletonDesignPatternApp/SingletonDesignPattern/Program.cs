8fbe107f6c9cbed26be9111c0da2fd34b93e82b8
Manish Jaiswal <jaiswal.manish49@gmail.com>
2023-03-31 02:47:42 +0530

    added singleton pattern

---
+++ SingletonDesignPatternApp/SingletonDesignPattern/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonDesignPattern;
+
+Singleton object1 = Singleton.GetInstance();
+
+Singleton object2 = Singleton.GetInstance();
---
+++ SingletonDesignPatternApp/SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    public class Singleton
+    {
+        private static int instanceCount = 0;
+
+        private static Singleton instance;
+
+        private static object lockObject = new Object();
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            lock (lockObject)
+            {
+                if (instance == null)
+                {
+                    instanceCount += 1;
+                    Console.WriteLine(instanceCount);
+                    instance = new Singleton();
+                }
+            }
+            return instance;
+           
+        }
+    }
+}
